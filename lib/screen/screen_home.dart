21d9b606304dfdbc93d2a99ff77637f33c09e91e
Enterprise09 <goldenstaytes@gmail.com>
2022-01-24 00:27:25 +0900

    [+] Home screen with & asset settings

---
+++ lib/screen/screen_home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    return SafeArea(
+      // return Safe app area
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Quiz App'),
+          backgroundColor: Colors.lightBlue,
+          leading: Container(), // Remove useless Appbar buttons
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Image.asset(
+                'images/quiz.jpeg',
+                width: width * 0.8,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
