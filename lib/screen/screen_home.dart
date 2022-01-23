109e5ff47cd4e4f99b72413b77b26bc2f49317ff
Enterprise09 <goldenstaytes@gmail.com>
2022-01-24 00:40:37 +0900

    [+] start quiz button & precautions

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -8,6 +8,26 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Widget _buildStep(double width, String title) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+          width * 0.048, width * 0.024, width * 0.048, width * 0.024),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.check_box,
+            size: width * 0.04,
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: width * 0.024),
+          ),
+          Text(title),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
@@ -32,6 +52,56 @@ class _HomeScreenState extends State<HomeScreen> {
                 width: width * 0.8,
               ),
             ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.024),
+            ),
+            Text(
+              'Flutter Quiz App',
+              style: TextStyle(fontSize: width * 0.065),
+              textAlign: TextAlign.center,
+            ),
+            const Text(
+              '퀴즈를 풀기 전 안내사항 입니다.\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            _buildStep(
+              width,
+              '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.',
+            ),
+            _buildStep(
+              width,
+              '2. 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.',
+            ),
+            _buildStep(
+              width,
+              '3. 만점을 향해 도전하세요!',
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: width * 0.036),
+              child: Center(
+                child: ButtonTheme(
+                  minWidth: width * 0.8,
+                  height: height * 0.05,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: RaisedButton(
+                    child: const Text(
+                      '지금 퀴즈 풀기',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.lightBlue,
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
